9ec15b4c351492095bb44ddec9ff580e88ee8f33
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-22 05:27:55 +0430

    init

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
   const [show, setShow] = useState(false)
+
+  return(
+    <>
+    
+    </>
+  )
 };
 
 export default ShowHide;
