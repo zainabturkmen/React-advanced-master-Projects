934eb05518c5fe57864bf6db3dced38447523085
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-01 05:53:31 +0430

    adding some changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Setup from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data"
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <Setup />
     </div>
   )
 }
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,3 +1,4 @@
+import { wait } from "@testing-library/user-event/dist/utils";
 import React, { useState, useEffect } from "react";
 
 const url = "https://api.github.com/users";
@@ -5,10 +6,15 @@ const url = "https://api.github.com/users";
 const UseEffectFetchData = () => {
   const [users, setUsers] = useState([]);
 
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
+  };
 
   useEffect(() => {
-    
-  })
+    getUsers();
+  }, []);
 
   return (
     <>
