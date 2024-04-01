ec12b40b5c5ba5d026538065d3cb50f568373651
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-01 06:09:44 +0430

    adding some changes

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -5,7 +5,24 @@ import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/zainabturkmen/followers?per_page=100";
 
 const UseEffectFetchData = () => {
- const [users, setUsers] = useState([])
+ const [users, setUsers] = useState([]);
+
+const githubUsers = async() => {
+  const response = await fetch(url);
+  const users = await response.json();
+  setUsers(users);
+}
+
+ useEffect(() => {
+
+ }, [])
+
+
+ return(
+  <>
+  <h3>github users</h3>
+  </>
+ )
 };
 
 export default UseEffectFetchData;
