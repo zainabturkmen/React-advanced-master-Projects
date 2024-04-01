596694d6917b758c42c7f932b5bbe0167025248b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-01 05:59:08 +0430

    adding some changes

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -22,9 +22,15 @@ const UseEffectFetchData = () => {
       <ul className="users">
         {users.map((user) => {
           const { id, login, avatar_url, html_url } = user;
-          return <li key={id}>
-            <img src={avatar_url} />
-          </li>;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                
+              </div>
+            </li>
+          );
         })}
       </ul>
     </>
