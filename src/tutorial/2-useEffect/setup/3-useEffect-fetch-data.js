65a7ceae280af2a542cdb1d3ad75c474cfc83e5d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-01 06:02:11 +0430

    adding some changes

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,7 +1,8 @@
 import { wait } from "@testing-library/user-event/dist/utils";
 import React, { useState, useEffect } from "react";
 
-const url = "https://api.github.com/users";
+// const url = "https://api.github.com/users";
+const url = "https://api.github.com/users/zainabturkmen/followers?per_page=100";
 
 const UseEffectFetchData = () => {
   const [users, setUsers] = useState([]);
@@ -27,7 +28,6 @@ const UseEffectFetchData = () => {
               <img src={avatar_url} alt={login} />
               <div>
                 <h4>{login}</h4>
-                
               </div>
             </li>
           );
