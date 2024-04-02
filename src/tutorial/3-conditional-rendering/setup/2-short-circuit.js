3f8f9f6f7b500f3e936f229ca3ba859e152c6971
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-02 21:00:40 +0430

    adding some changes

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
@@ -6,7 +6,8 @@ const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return <>
+  </>;
 };
 
 export default ShortCircuit;
