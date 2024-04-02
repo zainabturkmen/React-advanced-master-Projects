665ebfb36f1b243761f76baec812ce3ff3b42305
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-02 05:44:09 +0430

    adding some changes

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,13 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+  return <h2>Multiple Loading...</h2>
 };
 
 export default MultipleReturns;
