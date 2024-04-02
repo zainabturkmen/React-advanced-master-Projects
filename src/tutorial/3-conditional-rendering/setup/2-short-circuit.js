05415f17099793f12fae00e61d55141f8929a0a7
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-02 21:35:38 +0430

    adding some changes

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 // ternary operator
 
 const ShortCircuit = () => {
-  const [text, setText] = useState(".");
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
   // const firstValue = text || "hello world";
   // const secondValue = text && "hello world";
 
