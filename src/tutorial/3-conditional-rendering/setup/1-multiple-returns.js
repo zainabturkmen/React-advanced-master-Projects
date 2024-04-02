b9e3ffbc59ad9df0d9cdd8999e5dc2f896f8401f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-02 05:47:57 +0430

    adding some changes

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -3,11 +3,13 @@ const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturns = () => {
   const [loading, setLoading] = useState(true);
+  const [erroe, setError] = useState(false);
+  const [user, setUsers] = useState("default user");
 
   if (loading) {
     return <h2>Loading...</h2>;
   }
-  return <h2>Multiple Loading...</h2>
+  return <h2>Multiple Loading...</h2>;
 };
 
 export default MultipleReturns;
