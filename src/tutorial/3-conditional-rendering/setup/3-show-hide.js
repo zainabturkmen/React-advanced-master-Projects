a657e6db8cb89fbd575ce6eced47aad2a3fbadf4
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-03 05:36:37 +0430

    adding some changes

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -8,14 +8,17 @@ const ShowHide = () => {
       <button className="" btn onClick={() => setShow(!show)}>
         show/hide
       </button>
+      {show && <Item />}
     </>
   );
 };
 
 const Item = () => {
-  return <div style={{marginTop: "2rem"}}>
-    <h1>window</h1>
-    <h2>size : </h2>
-  </div>;
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <h1>window</h1>
+      <h2>size : </h2>
+    </div>
+  );
 };
 export default ShowHide;
