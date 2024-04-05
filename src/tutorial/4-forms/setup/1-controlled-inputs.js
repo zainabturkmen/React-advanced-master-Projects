32649f98772fe906266b6d86a54015faa771f3a5
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-05 23:08:45 +0430

    adding some changes

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -14,7 +14,7 @@ const ControlledInputs = () => {
   return (
     <>
       <article>
-        <form className="form" onSubmit={handleSubmit}>
+        <form className="form">
           <div className="form-control">
             <label htmlFor="firstName">Name : </label>
             <input type="text" id="firstName" name="firstName" />
@@ -23,7 +23,9 @@ const ControlledInputs = () => {
             <label htmlFor="email">Email : </label>
             <input type="text" id="email" name="email" />
           </div>
-          <button type="submit">add person</button>
+          <button type="submit" onClick={handleSubmit}>
+            add person
+          </button>
         </form>
       </article>
     </>
