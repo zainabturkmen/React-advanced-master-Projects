093b4b74238aecc16607a994690da5b00f71e75a
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-05 14:58:00 +0430

    adding some changes

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -7,7 +7,7 @@ import React, { useState } from "react";
 
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState("");
-  const [email, setEmail] = useState("")
+  const [email, setEmail] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("hello world");
@@ -18,7 +18,12 @@ const ControlledInputs = () => {
         <form className="form" onSubmit={handleSubmit}>
           <div className="form-control">
             <label htmlFor="firstName">Name : </label>
-            <input type="text" id="firstName" name="firstName" />
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="email">Email : </label>
