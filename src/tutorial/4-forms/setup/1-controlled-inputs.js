f7260d9281147184e3e6d371bd8a807b4791c1bf
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-05 15:09:27 +0430

    adding some changes

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -23,7 +23,7 @@ const ControlledInputs = () => {
               id="firstName"
               name="firstName"
               value={firstName}
-              onChange={(e) => setFirstName}
+              onChange={(e) => setFirstName()}
             />
           </div>
           <div className="form-control">
