ed53635e0960a837550fb5814e0525a5bcab0097
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-20 07:05:22 +0430

    adding some changes

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef, useRef } from "react";
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  
+
   const refContainer = useRef(null);
 
   const handleSubmit = (e) => {
