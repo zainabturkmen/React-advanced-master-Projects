de335533fa5b48ee24c391060e1e1f06f2109372
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-20 07:15:40 +0430

    adding some changes

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,25 +1,24 @@
-import React, { useRef, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-
   const refContainer = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventdefault();
   };
 
-  console.log(refContainer);
+  console.log(refContainer.current.value);
 
   return (
     <>
       <form className="form" onSubmit={handleSubmit}>
         <div>
           <input type="text" ref={refContainer} />
-          <button type="button">Submit</button>
+          <button type="submit">Submit</button>
         </div>
       </form>
     </>
