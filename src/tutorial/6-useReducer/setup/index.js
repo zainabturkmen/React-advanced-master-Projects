b3f1b840ad0da2158ccf4234af30c0c8c86ccb91
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-26 15:40:43 +0430

    adding some changes

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
+
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [people, setPeople] = useState()
+  return <h2>useReducer</h2>; 
 };
 
 export default Index;
