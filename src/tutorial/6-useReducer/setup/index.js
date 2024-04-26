6f639a550ba4ab9d48e63b96ac0b567d588b9436
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-26 16:00:30 +0430

    adding some changes

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -36,7 +36,7 @@ const Index = () => {
         <button type="submit">add</button>
       </form>
       {people.map((person)=> {
-        return <div key={id}></div>
+        return <div key={person.id}></div>
       })}
     </>
   );
