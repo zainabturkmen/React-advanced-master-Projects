f4e7ac4cc312f6661582720fa6471e7079a56a14
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-26 15:41:11 +0430

    adding some changes

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -5,7 +5,7 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  const [people, setPeople] = useState()
+  const [people, setPeople] = useState(data)
   return <h2>useReducer</h2>; 
 };
 
