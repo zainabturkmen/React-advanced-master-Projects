6542deb171cc8b08bfb1f33135acd498dcadcf9e
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-26 15:50:22 +0430

    adding some changes

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,22 +1,31 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 
 // reducer function
 
 const Index = () => {
-  const [name, setName] = useState("")
-  const [people, setPeople] = useState(data)
-  const [showModal, setShowModal] = useState(false)
-  const []
-  return <>
-  {showModal && <Modal />}
-  <form onSubmit={handleSubmit}>
-    <div >
-      <input type='text' value={name} onChange={() => setName(!showModal)}/>
-    </div>
-  </form>
-  </>
+  const [name, setName] = useState("");
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <>
+      {showModal && <Modal />}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default Index;
