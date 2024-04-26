6afc6521dfc16fdc7b84a0b3ff53ee306af64b7f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-26 16:02:03 +0430

    adding some changes

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -12,13 +12,13 @@ const Index = () => {
   const handleSubmit = (e) => {
     e.preventDefault()
     if (name) {
-      showModal(true);
+      setShowModal(true);
       setPeople([...people,{id:new Date().getTime().toString(), name}])
       setName("")
     }
 
     else{
-      showModal(true)
+      setShowModal(true)
     }
   }
 
