b77e12ca8b011699c099a64eeb22168f881a5e0d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-27 17:26:23 +0430

    adding some changes

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -40,6 +40,7 @@ const SinglePerson = ({ id, name  , removePerson}) => {
   return (
     <div className="item">
       <h4>{name}</h4>
+      <button onClick={()=> removePerson(id)}>remove</button>
     </div>
   );
 };
