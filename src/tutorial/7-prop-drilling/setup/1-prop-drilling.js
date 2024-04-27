88271ee9a42de89895c995aa596e9c42ff95978b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-27 16:43:00 +0430

    adding some changes

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; 
 
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
   return <h2>prop drilling</h2>;
-};
+}; 
 
 export default PropDrilling;
