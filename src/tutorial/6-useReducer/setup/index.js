f9adb5c55d96eb05dc3365dbf9c3d15146a1777c
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-27 11:21:49 +0430

    adding some changes

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -6,7 +6,7 @@ import { type } from "@testing-library/user-event/dist/type";
 // reducer function
 
 const reducer = (state, action) => {
-  console.log(state, action);
+  if(action.type === "TESTING")
   return state; 
 }
 
