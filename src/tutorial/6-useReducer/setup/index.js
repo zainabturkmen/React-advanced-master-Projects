7b6341f33255028c7c3ea72e069317902522b921
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-27 16:15:46 +0430

    adding some changes

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -73,7 +73,7 @@ const Index = () => {
         return (
           <div key={person.id}>
             <h4>{person.name}</h4>
-            <button onClick={()=> dispatch({type:""})}></button>
+            <button onClick={()=> dispatch({type:"REMOVE_ITEM", })}></button>
           </div>
         );
       })}
