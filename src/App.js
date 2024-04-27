50944c36e0ab6ffe3ba77e7999747da6ce5115de
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-27 17:50:21 +0430

    adding some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
+import Setup from "./tutorial/8-useContext/setup/1-context-api";
 
 function App() {
   return (
