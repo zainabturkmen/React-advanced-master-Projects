67a223f236db14912aaeef8a5c90e465f8767f37
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-27 18:25:02 +0430

    adding some changes

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -13,10 +13,10 @@ const ContextAPI = () => {
     });
   };
   return (
-    <>
+    <PersonContext.Provider value='hello'>
       <h3>prop drilling</h3>
       <List people={people} removePerson={removePerson} />
-    </>
+    </PersonContext.Provider>
   );
 };
 
@@ -37,6 +37,7 @@ const List = ({ people, removePerson }) => {
 };
 
 const SinglePerson = ({ id, name, removePerson }) => {
+  const data = useContext()
   return (
     <div className='item'>
       <h4>{name}</h4>
