54071fea06b66ea5796759cc510113c2b2af2b12
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-27 16:11:25 +0430

    adding some changes

--- src/tutorial/6-useReducer/setup/Modal.js
+++ src/tutorial/6-useReducer/setup/Modal.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react";
 
 const Modal = ({ modalContent, closeModal }) => {
+  
+  useEffect(()=> {
+    
+  })
   return (
     <div className="modal">
       <p>{modalContent}</p>
