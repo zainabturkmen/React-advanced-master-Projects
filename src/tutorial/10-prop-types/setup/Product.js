eaff0fd7019504f55de5c45c0015107e0cde6fe5
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-30 12:11:52 +0430

    adding some changes

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import PropTypes from "prop-types"
 
 const Product = ({image,name, price }) => {
+
   return <article className='product'>
     <h4>Single products</h4>
     
